db6361b5f7f65374abf77b8acb798f77e780911f
Prabhjyot Singh Gill <>
2022-03-24 15:47:29 -0400

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 function App() {
   return (
-    <div className="app">
+    <div className="app container mt-5">
+      <h1 className="text-primary"><i className="bi bi-diagram-2-fill" ></i> Repos </h1>
     </div>
   );
 }
