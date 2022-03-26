53f0857b08ea078b6a8ecbbc293e0d4ed4dcbb8e
Prabhjyot Singh Gill <>
2022-03-25 21:55:48 -0400

    setting up graphql

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import github from "./db.js";
+import React, { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() =>{
+    const githubQuery = {
+       query:`
+       {
+         viewer{
+           name
+         }
+       }
+       `
+    }
+  })
+    
   return (
     <div className="app container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill" ></i> Repos </h1>
@@ -8,3 +23,4 @@ function App() {
 }
 
 export default App;
+
