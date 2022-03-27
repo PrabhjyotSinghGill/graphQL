4e8ef407a10a7594e2586e4148b8ca5f94a99995
Prabhjyot Singh Gill <>
2022-03-27 10:34:18 -0400

    useState hook to show info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import github from "./db.js";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+  let [userName, setUserName] = useState("");
   useEffect(() =>{
     const githubQuery = {
        query:`
@@ -11,13 +12,26 @@ function App() {
            name
          }
        }
-       `
-    }
-  })
+       `,
+    };
+    fetch(github.baseURL,{
+      method:"POST",
+      headers:github.headers,
+      body:JSON.stringify(githubQuery)
+    }).then(response => response.json())
+    .then(data =>{
+      setUserName(data.data.viewer.name)
+      console.log(data)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  });
     
   return (
     <div className="app container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill" ></i> Repos </h1>
+      <p>Hey there {userName}</p>
     </div>
   );
 }
