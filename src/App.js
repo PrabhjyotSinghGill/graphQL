537ab4f2dd801cf22c7d57468a2f4b9f8b3e24cf
Prabhjyot Singh Gill <>
2022-03-27 21:13:21 -0400

    generating a list of repositories

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import query from "./Query";
 
 function App() {
   let [userName, setUserName] = useState("");
-
+  let [repoList, setRepoList] = useState(null);
   const fetchData = useCallback(()=>{
     fetch(github.baseURL,{
       method:"POST",
@@ -12,7 +12,9 @@ function App() {
       body:JSON.stringify(query),
     }).then(response => response.json())
     .then(data =>{
-      setUserName(data.data.viewer.name)
+      const viewer = data.data.viewer;
+      setUserName(viewer.name);
+      setRepoList(viewer.repositories.nodes);
       console.log(data)
     })
     .catch(err =>{
@@ -30,6 +32,22 @@ function App() {
     <div className="app container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill" ></i> Repos </h1>
       <p>Hey there {userName}</p>
+      {
+        repoList && (
+          <ul className="list-group list-group-flush">
+            {
+              repoList.map((repo) =>(
+                <li className="list-group-item" key={repo.id.toString()}>
+                  <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                    {repo.name}
+                  </a>
+                  <p className="small">{repo.description}</p>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
     </div>
   );
 }
